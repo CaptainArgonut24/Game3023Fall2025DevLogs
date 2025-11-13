62909a0b60e2e8e7b85751135c82d20cf026c18a
John <johnhusky400@gmail.com>
2025-11-12 20:07:02 -0500

    just need to reed add the data and fix size

--- GAME3023_Midterm_F2025JohnHusky(101426515)/Assets/InventorySystem/Scripts/Drag/InventorySlot.cs
+++ GAME3023_Midterm_F2025JohnHusky(101426515)/Assets/InventorySystem/Scripts/Drag/InventorySlot.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 public class InventorySlot : MonoBehaviour, IDropHandler
 {
+
     public void OnDrop(PointerEventData eventData)
     {
         if (transform.childCount == 0)
